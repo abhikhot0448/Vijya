e22ba36b1a6d761528c0ca4cff9154439d63270b
Abhimanyui <abhikhot1711@gmail.com>
2022-07-31 23:26:34 +0530

    abhi khot

---
+++ server.js
@@ -0,0 +1,44 @@
+
+
+var express=require('express');
+var app=express();
+
+app.get("/",(req,res)=>{
+    res.send(
+        "<h1>Vijay Sales </h1>"
+        +"<hr/>"
+        +"<h3>Smart devices for sale</h3>"
+        +"<br/>"
+        +"<ol>"
+        +"<li>Laptop</li>"
+        +"<li>Mobile</li>"
+        +"<li>Tablet</li>"
+        +"</ol>"
+        );
+});
+
+app.get("/login",(req,res)=>{
+    res.send(
+        "<h1>Vijay Sales </h1>"
+        +"<hr/>"
+        +"<form>"
+        +"<input/>"
+        +"<br/>"
+        +"<button><>Login</button>"
+        +"</form>"
+        );
+});
+
+app.get("/aboutus",(req,res)=>{
+    res.send(
+        "<h1>Vijay Sales </h1>"
+        +"<hr/>"
+        +"<h3>it is information about us</h3>"
+        +"<br/>"
+        +"<ol>"
+        +"<li>tambde sir</li>"
+        +"<li>Pranit khamkar</li>"
+        +"<li>Yash Korane</li>"
+        +"</ol>"
+        );
+});
